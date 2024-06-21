e8b4fd3fa4247df62174fae3caa64abca8dc4037
traccoenlea <leatraccoen@gmail.com>
2024-06-21 11:44:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const startingSpots = [
+    { id: 0, name: "tête de raquette" },
+    { id: 1, name: "45°" },
+    { id: 2, name: "lancer-franc" },
+    { id: 3, name: "90°" },
+    { id: 4, name: "0°" },
+  ];
+
+  const finitions = [
+    { id: 0, name: "shoot" },
+    { id: 1, name: "push up" },
+    { id: 2, name: "extra step" },
+    { id: 3, name: "lay up" },
+    { id: 4, name: "lay back" },
+    { id: 5, name: "power" },
+    { id: 6, name: "euro step" },
+  ];
+
+  const endingSpots = [
+    { id: 0, name: "0°" },
+    { id: 1, name: "3pts" },
+    { id: 2, name: "raquette" },
+    { id: 3, name: "2pts" },
+  ];
+
+  const dribbles = [
+    { id: 0, name: 0 },
+    { id: 1, name: 1 },
+    { id: 2, name: 2 },
+    { id: 3, name: 3 },
+    { id: 4, name: 4 },
+    { id: 5, name: 5 },
+    { id: 6, name: "illimité" },
+  ];
+
+  const bonus = [
+    { type: "bonus", name: "test bonus" },
+    { type: "malus", name: "test malus" },
+  ];
+
+  const constraints = [
+    { id: 0, name: "Constraint 1" },
+    { id: 1, name: "Constraint 2" },
+  ];
+
+  const arrays = [startingSpots, finitions, endingSpots, dribbles, constraints];
+
+  function getRandomname(array) {
+    const size = array.length;
+    const random = Math.floor(Math.random() * size);
+    const dataToReturn = array[random];
+    return dataToReturn;
+  }
+
+  function handleBtn() {
+    setData([]);
+    for (let i = 0; i < arrays.length; i++) {
+      const tab = arrays[i];
+      const random = getRandomname(tab);
+      setData((oldArray) => [...oldArray, random]);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleBtn}>Générer le 1v1</button>
+
+      {data.map((d, i) => (
+        <h3 key={i}> {d.name}</h3>
+      ))}
     </div>
   );
 }
